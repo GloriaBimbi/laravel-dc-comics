86bc02eae7ae30e55831d63289711ee1f64efb4a
GloriaBimbi <gloria.bimbi1@libero.it>
2024-03-25 17:05:31 +0100

    feat index()

--- README.md
+++ README.md
@@ -1,32 +1,16 @@
-<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
+## Milestone 1
 
-<p align="center">
-<a href="https://github.com/laravel/framework/actions"><img src="https://github.com/laravel/framework/workflows/tests/badge.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
-</p>
+Tramite gli appositi comandi artisan create un model con relativa migration e un resource controller.
 
-## Init project
+## Milestone 2
 
-Per il frontend:
+Iniziate a definire le prime operazioni CRUD con le relative view:
 
-```
-npm install
-npm run dev
-```
+-   index()
+-   show()
+-   create()
+-   store()
 
-Per il backend:
+## Bonus:
 
-```
-composer install
-php artisan serve
-```
-
-Poi copia il file `.env.example` in `.env` e configura la connessione al DB
-
-## Create a controller
-
-...
-
-## Create a model
+creare il seeder per la tabella comics utilizzando il file fornito.
--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -10,17 +10,16 @@ class ComicController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $comics = Comic::all();
+        return view('home', compact('comics'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -31,7 +30,6 @@ class ComicController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -42,7 +40,6 @@ class ComicController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Comic  $comic
-     * @return \Illuminate\Http\Response
      */
     public function show(Comic $comic)
     {
@@ -53,7 +50,6 @@ class ComicController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Comic  $comic
-     * @return \Illuminate\Http\Response
      */
     public function edit(Comic $comic)
     {
@@ -65,7 +61,6 @@ class ComicController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Comic  $comic
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Comic $comic)
     {
@@ -76,7 +71,6 @@ class ComicController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Comic  $comic
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Comic $comic)
     {
